4f8320cc954b082beece451ec7f7da0137f8ac62
Breno Farias <57010178+BrenoFariasdaSilva@users.noreply.github.com>
2023-06-09 17:51:56 -0300

    Fixing identations

--- client/src/TextEditor.js
+++ client/src/TextEditor.js
@@ -4,8 +4,8 @@ import Quill from "quill";
 import "quill/dist/quill.snow.css";
 
 export default function TextEditor() {
-    useEffect(() => { // useEffect is a hook that runs when the component is mounted. This makes it so that the editor is only rendered once the component is mounted.
-        new Quill("#container", { theme: "snow" }) // This creates a new Quill object and renders it in the div with id="container".
-    }, [])
-    return <div id="container"></div>
+   useEffect(() => { // useEffect is a hook that runs when the component is mounted. This makes it so that the editor is only rendered once the component is mounted.
+      new Quill("#container", { theme: "snow" }) // This creates a new Quill object and renders it in the div with id="container".
+   }, [])
+   return <div id="container"></div>
 }
