f43938906ee2dda124e1859ff73b3dc88c2b40b1
Breno Farias <57010178+BrenoFariasdaSilva@users.noreply.github.com>
2023-06-10 14:45:15 -0300

    DOCS: Updating client/TextEditor.js documentation

--- client/src/TextEditor.js
+++ client/src/TextEditor.js
@@ -1,8 +1,12 @@
+// Import the necessary modules from the 'react' package
 import React from "react";
 import { useCallback } from "react";
+
+// Import the 'Quill' library and its styles
 import Quill from "quill";
 import "quill/dist/quill.snow.css";
 
+// Define the toolbar options for the Quill editor
 const TOOLBAR_OPTIONS = [
   [{ header: [1, 2, 3, 4, 5, 6, false] }],
   [{ font: [] }],
@@ -13,16 +17,28 @@ const TOOLBAR_OPTIONS = [
   [{ align: [] }],
   ["image", "blockquote", "code-block"],
   ["clean"],
-]
+];
 
+// Define the 'TextEditor' component
 export default function TextEditor() {
-   const wrapperRef = useCallback((wrapper) => { // This creates a reference to the div with id="container".
-      if (wrapper == null) return; // If the div with id="container" does not exist, then return.
+  // Create a callback using 'useCallback' to store a reference to the container div
+  const wrapperRef = useCallback((wrapper) => {
+    // If the div with id="container" does not exist, then return
+    if (wrapper == null) return;
 
-      wrapper.innerHTML = ""; // This clears the div with id="container".
-      const editor = document.createElement("div"); // This creates a div element.
-      wrapper.append(editor); // This appends the div element to the div with id="container".
-      new Quill(editor, { theme: "snow", modules: {toolbar: TOOLBAR_OPTIONS} }); // This creates a Quill object.
-   }, []);
-   return <div className="container" ref={wrapperRef}></div>
+    // Clear the content of the container div
+    wrapper.innerHTML = "";
+
+    // Create a new div element for the Quill editor
+    const editor = document.createElement("div");
+
+    // Append the Quill editor div to the container div
+    wrapper.append(editor);
+
+    // Create a new Quill object with specified options
+    new Quill(editor, { theme: "snow", modules: { toolbar: TOOLBAR_OPTIONS } });
+  }, []);
+
+  // Render the container div and assign the wrapperRef callback as the 'ref' attribute
+  return <div className="container" ref={wrapperRef}></div>;
 }
