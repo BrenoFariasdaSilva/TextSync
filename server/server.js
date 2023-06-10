dae9b92e06324c674633a3aa46c6f0e576a17b40
Breno Farias <57010178+BrenoFariasdaSilva@users.noreply.github.com>
2023-06-10 14:38:39 -0300

    DOCS: Updating server documentation

--- server/server.js
+++ server/server.js
@@ -1,10 +1,18 @@
+// Desc: Server for the TextSync - A Realtime Distributed Text Editor.
+
+// This loads the .env file from the root directory of the server.
 require('dotenv').config({ path: './.env' });
 
+// Import the 'socket.io' package
 const io = require('socket.io')(server, {
+    // Configure Cross-Origin Resource Sharing (CORS)
     cors: {
-        origin: "http://localhost:3000",
-        methods: ["GET", "POST"]
+      // Define the allowed origin for cross-origin requests
+      origin: "http://localhost:3000",
+      // Specify the allowed HTTP methods for cross-origin requests
+      methods: ["GET", "POST"]
     }
-});
-
+  });
+  
+// io.on is a listener for incoming connections
 io.on('connection', (socket) => {});
