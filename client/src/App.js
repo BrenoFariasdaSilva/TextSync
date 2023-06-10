b0473cefdf3f5376b4044305dff272c2532544c7
Breno Farias <57010178+BrenoFariasdaSilva@users.noreply.github.com>
2023-06-10 14:43:36 -0300

    DOCS: Updating client/App.js documentation

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,14 @@
+// Import the necessary module from the 'react' package
 import React from "react";
+
+// Import the 'TextEditor' component from the './TextEditor' file
 import TextEditor from "./TextEditor";
 
+// Define the 'App' component
 function App() {
+   // Render the 'TextEditor' component
    return <TextEditor />;
 }
 
+// Export the 'App' component as the default export
 export default App;
