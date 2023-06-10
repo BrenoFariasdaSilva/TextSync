0d597952e94b4f2a6c6875f35783f177099d5b4e
Breno Farias <57010178+BrenoFariasdaSilva@users.noreply.github.com>
2023-06-10 14:33:09 -0300

    FEAT: Create a websocket connection

--- server/server.js
+++ server/server.js
@@ -1,1 +1,8 @@
 require('dotenv').config({ path: './.env' });
+
+const io = require('socket.io')(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"]
+    }
+});
