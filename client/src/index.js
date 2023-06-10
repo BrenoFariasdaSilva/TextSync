0f97215ef2f5165d4a2f694849d6a0c5d6077555
Breno Farias <57010178+BrenoFariasdaSilva@users.noreply.github.com>
2023-06-10 14:42:54 -0300

    DOCS: Updating client/index.js documentation

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,19 @@
+// Import the necessary modules from the 'react' and 'react-dom' packages
 import React from "react";
 import ReactDOM from "react-dom/client";
+
+// Import the main component of the application from the './App' file
 import App from "./App";
+
+// Import the CSS styles from the './styles.css' file
 import "./styles.css";
 
+// Create a root React element using 'ReactDOM.createRoot' and specify the DOM element to mount the application
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
+// Render the application inside the root element
 root.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
