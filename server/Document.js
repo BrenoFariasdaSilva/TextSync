c33c685e400ffc3897a7882110652589a4c2eca8
Breno Farias <57010178+BrenoFariasdaSilva@users.noreply.github.com>
2023-06-12 18:56:55 -0300

    FEAT: Creating fields _id and data for the Schema

--- server/Document.js
+++ server/Document.js
@@ -1,1 +1,11 @@
-const { Schema, model } = require('mongoose');
+// Desc: Document model for MongoDB
+const { Schema, model } = require("mongoose");
+
+// Define the schema for the Document model
+const Document = new Schema({
+	_id: String, // id of the document
+	data: Object // data of the document
+});
+
+// Export the Document model
+module.exports = model("Document", Document);
