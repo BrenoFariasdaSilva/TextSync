c7aabb285058cfa46fc3f6fb937d81c595d43f18
Breno Farias <brenofarias@Brenos-MacBook-Pro.local>
2023-06-13 14:24:08 -0300

    FIX: Fixing the broadcast error

--- server/server.js
+++ server/server.js
@@ -9,42 +9,58 @@ const mongoose = require("mongoose");
 const Document = require("./Document");
 // Connect to the MongoDB database
 mongoose.connect(process.env.DATABASE_URI, {
-   useNewUrlParser: true,
-   useUnifiedTopology: true
-}).then(() => console.log("MongoDB connected successfully"))
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => console.log("MongoDB connected successfully"));
 
 // Import the 'http' package
 const server = require("http").createServer();
 server.listen(process.env.SERVER_PORT, () => {
-   console.log(`Server listening on port ${process.env.SERVER_PORT}`);
+  console.log(`Server listening on port ${process.env.SERVER_PORT}`);
 });
 
 // Import the 'socket.io' package
 const io = require("socket.io")(server, {
-   // Configure Cross-Origin Resource Sharing (CORS)
-   cors: {
-   // Define the allowed origin for cross-origin requests
-   origin: `${process.env.CLIENT_ADDRESS}:${process.env.CLIENT_PORT}`,
-   // Specify the allowed HTTP methods for cross-origin requests
-   methods: ["GET", "POST"],
-   }
+  // Configure Cross-Origin Resource Sharing (CORS)
+  cors: {
+    // Define the allowed origin for cross-origin requests
+    origin: `${process.env.CLIENT_ADDRESS}:${process.env.CLIENT_PORT}`,
+    // Specify the allowed HTTP methods for cross-origin requests
+    methods: ["GET", "POST"],
+  },
 });
-  
+
+// Store the active document sockets
+const documentSockets = {};
+
 // io.on is a listener for incoming connections
-io.on("connection", socket => { // socket is an object that represents the client
-   socket.on("get-document", async documentId => { // documentId is the id of the document
-      const document = await findOrCreateDocumentById(documentId); // find or create the document by its id
-      socket.join(documentId); // join the room identified by the documentId
-      socket.emit("load-document", document.data); // emit a 'load-document' event to the client
-   });
+io.on("connection", socket => {
+  socket.on("get-document", async documentId => {
+    const document = await findOrCreateDocumentById(documentId);
+    socket.join(documentId);
+    socket.emit("load-document", document.data);
+    
+    // Associate the document ID with the socket
+    documentSockets[socket.id] = documentId;
+  });
 
-   socket.on("send-changes", delta => { // delta is the change in the text
-      socket.broadcast.emit("receive-changes", delta); // broadcast the change to all other clients, except the sender
-   });
+  socket.on("send-changes", delta => {
+    // Get the document ID associated with the socket
+    const documentId = documentSockets[socket.id];
+    if (documentId) {
+      // Broadcast the changes only to clients in the same document room
+      socket.to(documentId).emit("receive-changes", delta);
+    }
+  });
 
-   socket.on("save-document", async data => { // data is the document data
-      await Document.findByIdAndUpdate(data._id, { data }); // update the document data
-   });
+  socket.on("save-document", async data => {
+    await Document.findByIdAndUpdate(data._id, { data });
+  });
+
+  socket.on("disconnect", () => {
+    // Remove the document ID association when the socket disconnects
+    delete documentSockets[socket.id];
+  });
 });
 
 // Define the default value for the document data
@@ -52,14 +68,10 @@ const defaultValue = "";
 
 // Desc: Find or create a document by its id
 async function findOrCreateDocumentById(id) {
-   if (id == null) return; // if id is null, return
+  if (id == null) return;
 
-   // Find the document by its id
-   const document = await Document.findById(id);
+  const document = await Document.findById(id);
+  if (document) return document;
 
-   // If the document exists, return it
-   if (document) return document;
-
-   // If the document does not exist, create it
-   return await Document.create({ _id: id, data: defaultValue });
+  return await Document.create({ _id: id, data: defaultValue });
 }
