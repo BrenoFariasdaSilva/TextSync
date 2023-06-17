cca942d04b8ceb382bae2cc0e7fbdb11ab718c38
Breno Farias <57010178+BrenoFariasdaSilva@users.noreply.github.com>
2023-06-17 15:43:16 -0300

    REFACTOR

--- docker/installation.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Download the Docker Image
-docker pull mongo
-
-# Create a Docker Container with the Mongo Image
-docker run --name MongoDB --restart unless-stopped -d -p 3010:3010 mongo
